6f59c961cf5f89caa7739a07f617e3dc8256bc00
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-02-28 17:12:35 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 305 ms, Memory: 100,276 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,6 +1,11 @@
+
+// 위에서부터 내려오기
 import java.util.Scanner;
 
 public class Solution {
+
+//	 좌, 우, 하
+	static int[][] delta = { { 0, -1 }, { 0, 1 }, { 1, 0 } };
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
@@ -9,43 +14,41 @@ public class Solution {
 			int T = sc.nextInt();
 
 			int[][] arr = new int[100][100];
-			int x = 0;
-			int y = 99;
 			for (int i = 0; i < 100; i++) {
 				for (int j = 0; j < 100; j++) {
 					arr[i][j] = sc.nextInt();
-					if (i == 99 && arr[i][j] == 2) {
-						x = j;
+				}
+			}
+
+			int answer = -1;
+
+			for (int i = 0; i < 100; i++) {
+				if (arr[0][i] != 1)
+					continue;
+
+				boolean[][] visit = new boolean[100][100];
+				int r = 0;
+				int c = i;
+
+				while (!visit[r][c] && r < 99) {
+					visit[r][c] = true;
+					for (int d = 0; d < 3; d++) {
+						int nr = r + delta[d][0];
+						int nc = c + delta[d][1];
+						if (nr >= 0 && nr < 100 && nc >= 0 && nc < 100 && !visit[nr][nc] && arr[nr][nc] != 0) {
+							r = nr;
+							c = nc;
+							break;
+						}
+					}
+					if (arr[r][c] == 2) {
+						answer = i;
+						break;
 					}
 				}
 			}
 
-			while (y != 0) {
-
-				// 좌
-				if (x - 1 >= 0 && arr[y][x - 1] == 1) {
-					while (x - 1 >= 0 && arr[y][x - 1] == 1) {
-						x--;
-					}
-				}
-
-				// 우
-				else if (x + 1 <= 99 && arr[y][x + 1] == 1) {
-					while (x + 1 <= 99 && arr[y][x + 1] == 1) {
-						x++;
-					}
-				}
-
-				// 상
-				y--;
-
-				if (y == 0) {
-					break;
-				}
-			}
-
-			System.out.println("#" + T + " " + x);
+			System.out.println("#" + T + " " + answer);
 		}
 	}
 }
-// 뒤에서부터 올라가기
