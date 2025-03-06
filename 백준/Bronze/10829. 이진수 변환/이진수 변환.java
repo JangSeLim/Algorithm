b5974243432a854799f25ac3d3106f21accfe20a
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-06 17:24:15 +0900

    [Bronze II] Title: 이진수 변환, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/10829. 이진수 변환/이진수 변환.java
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.java
@@ -1,21 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long N = Long.parseLong(br.readLine());
-        System.out.println(toBinary(N));
-    }
-
-    static String toBinary(long n) {
-        StringBuilder sb = new StringBuilder();
-        
-        while (n > 0) {
-        	sb.append(n % 2);
-        	n /= 2;
-        }
-        return sb.reverse().toString();
+        long number = Long.parseLong(br.readLine());
+        System.out.print(Long.toString(number, 2));
+        br.close();
     }
 }
