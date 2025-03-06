0ef7d3be05087f1447917596238d924f51b71827
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-06 11:48:56 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 184 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-	
-	static int[][] memo;
-
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int i = 0; i < T; i++) {
-			int k = sc.nextInt();
-			int n = sc.nextInt();
-			memo = new int[k+1][n+1];
-
-			int answer = number(k, n);
-			System.out.println(answer);
-		}
-	}
-
-	// a층 b호에 사는 사람들의 수를 구하는 함수
-	static int number(int a, int b) {
-		if (a == 0) return b;
-		
-		if (memo[a][b] != 0) {
-			return memo[a][b];
-		}
-		
-		int sum = 0;
-		// a - 1층의 1호부터 b호까지 사는 사람들을 다 더해야함
-		for (int i = 1; i <= b; i++) {
-			sum += number(a - 1, i);
-		}
-
-		return memo[a][b] = sum;
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int k = sc.nextInt(); // k층
+            int n = sc.nextInt(); // n호
+            
+            int[] people = new int[n];
+            for (int j = 0; j < n; j++) {
+                people[j] = j + 1; // 0층 초기화
+            }
+            
+            for (int x = 0; x < k; x++) {
+                for (int y = 1; y < n; y++) {
+                    people[y] += people[y - 1];
+                }
+            }
+            
+            System.out.println(people[n - 1]);
+        }
+        
+        sc.close();
+    }
 }
