bbcb4c14fa0e1745b71f9c2af706fa60f0f33f22
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-10 17:37:09 +0900

    [D3] Title: 이진수 표현, Time: 285 ms, Memory: 49,492 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -1,22 +1,25 @@
 
 import java.util.Scanner;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
+public class Solution {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int T = sc.nextInt();
-		for (int test_case = 1; test_case <= T; test_case++) {
-			System.out.print("#" + test_case + " ");
+		for (int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-			
-			if ((M & ((1 << N) - 1)) == (1 << N) - 1) {
-				System.out.print("ON");
+
+			StringBuilder sb = new StringBuilder();
+
+			if ((M & ((1 << N)-1)) == ((1 << N) - 1)) {
+				sb.append("ON");
 			} else {
-				System.out.print("OFF");
+				sb.append("OFF");
 			}
-			System.out.println();
-		}
-		sc.close();
+			
+			System.out.println("#" + tc + " " + sb.toString());
+		} // tc
+
 	}
 }
