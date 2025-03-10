7cc65d51c39e7a686f84259f8ae144b5e9936221
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-10 21:33:16 +0900

    [D3] Title: 부분 수열의 합, Time: 2,588 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,39 +1,42 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
+    static int N, K;
+    static int[] arr;
+    static int count;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 케이스 개수
 
-        int T = sc.nextInt();
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int N = sc.nextInt();
-            int K = sc.nextInt();
-            int[] arr = new int[N];
+        for (int t = 1; t <= T; t++) {
+            N = sc.nextInt(); // 배열 크기
+            K = sc.nextInt(); // 목표 합
+            arr = new int[N];
+
             for (int i = 0; i < N; i++) {
-            	arr[i] = sc.nextInt();
+                arr[i] = sc.nextInt();
             }
-            
-            int cnt = 0;
-            int groupCnt = 1 << N;
-            for(int k = 1; k < groupCnt; k++) {
-            	int sum = 0;
-            	
-            	for (int i = 0; i <N; i++) {
-            		if ((k & (1 << i)) != 0) {
-            			sum += arr[i];
-            		}
-            	}
-            	if (sum == K) {
-            		cnt++;
-            	}
-            }
-            
-            System.out.println("#" + test_case + " " + cnt);
-            
+
+            count = 0;
+            findSubsets(0, 0);
+
+            System.out.println("#" + t + " " + count);
         }
         sc.close();
-    
     }
+
+    static void findSubsets(int idx, int sum) {
+        if (idx == N) {
+            return;
+        }
+
+        // 현재 요소 선택하는 경우
+        if (sum + arr[idx] == K) {
+            count++;
+        }
+        findSubsets(idx + 1, sum + arr[idx]); // 포함하는 경우
+        findSubsets(idx + 1, sum); // 포함하지 않는 경우
+    }
+
 }
