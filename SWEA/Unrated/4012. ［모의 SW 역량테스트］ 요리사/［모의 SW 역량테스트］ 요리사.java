8a1a0ef9637d789ca060a6ba114b4c5477bf7b11
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-11 14:30:33 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 528 ms, Memory: 42,948 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,71 +1,50 @@
-
-import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution {
-	
-	static boolean[] visited;
-	static int N, minDiff;
-	static int[][] arr;
-	
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int test_case = 1; test_case <= T; test_case++) {
-			N = sc.nextInt();
-			arr = new int[N][N];
-			minDiff = Integer.MAX_VALUE;
-			visited = new boolean[N];
-			for (int i = 0; i <N; i++) {
-				for (int j = 0; j<N;j++) {
-					arr[i][j] = sc.nextInt();
-				}
-			}
-			
-			comb(0, 0);
-			
-			System.out.println("#" + test_case + " " + minDiff);
-		}
-	}
-	
-	public static void comb(int idx, int cnt) {
-		if (cnt == N/ 2) {
-			// 그룹 나누기
-			int sumA = 0;
-			int sumB = 0;
-			for (int i = 0; i <N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (visited[i] && visited[j]) {
-						sumA += arr[i][j];
-					}
-					if (!visited[i] && !visited[j]) {
-						sumB += arr[i][j];
-					}
-				}
-			}
-			
-			// minDiff 갱신
-			minDiff = Math.min(minDiff, Math.abs(sumA - sumB));
-			
-			return;
-		}
-		
-		for (int i = idx; i < N; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				comb(i+1, cnt+1);
-				visited[i] = false;
-			}
-		}
-	}
+public class Solution {
+
+    static int N, minDiff;
+    static int[][] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            N = sc.nextInt();
+            arr = new int[N][N];
+            minDiff = Integer.MAX_VALUE;
+
+            // 점수 입력 받기
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+
+            // 비트마스크를 사용하여 조합 탐색
+            for (int mask = 0; mask < (1 << N); mask++) {  // 2^N개의 경우를 확인
+                // 비트마스크의 1의 개수가 N/2와 같은 경우만 유효한 팀 분배
+                if (Integer.bitCount(mask) == N / 2) {
+                    int sumA = 0, sumB = 0;
+
+                    // 각 팀의 점수 합 계산
+                    for (int i = 0; i < N; i++) {
+                        for (int j = 0; j < N; j++) {
+                            if (((mask >> i) & 1) == 1 && ((mask >> j) & 1) == 1) {
+                                sumA += arr[i][j];  // 팀 A
+                            } else if (((mask >> i) & 1) == 0 && ((mask >> j) & 1) == 0) {
+                                sumB += arr[i][j];  // 팀 B
+                            }
+                        }
+                    }
+
+                    // 최소 차이값 갱신
+                    minDiff = Math.min(minDiff, Math.abs(sumA - sumB));
+                }
+            }
+
+            // 결과 출력
+            System.out.println("#" + tc + " " + minDiff);
+        }
+    }
 }
-
-// 식재료를 N/2개씩 나누어서 두 개의 요리
-// A음식과 B음식의 맛 차이를 최소가 되도록 배분
-// 두음식간의 맛의 차이가 최소가 되는 경우
-
-// N/2개 선택하는 조합 문제
-// 1. N길이의 방문 배열 생성
-// 2. N개 중에 N/2개 선택하는 조합 -> 나머지는 자동으로 B그룹
-// 3. 각 그룹의 시너지 저장
-// 4. 두 그룹의 맛 차이 계산, 최솟값 갱신
