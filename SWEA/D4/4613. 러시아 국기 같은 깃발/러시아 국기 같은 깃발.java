de17714cca927498609e33878d8412e1b6b850cb
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-11 14:48:50 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 119 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -3,65 +3,58 @@ import java.util.Scanner;
 class Solution {
     static int N, M;
     static char[][] colors;
-    static int minCnt; // 최소 색칠 횟수
+    static int[][] colorCount;
+    static int minCnt;
 
     public static void main(String args[]) throws Exception {
-
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt(); // 테스트 케이스 수
-
+        int T = sc.nextInt();
         for (int test_case = 1; test_case <= T; test_case++) {
-            N = sc.nextInt(); // N: 행의 개수
-            M = sc.nextInt(); // M: 열의 개수
+            N = sc.nextInt();
+            M = sc.nextInt();
             colors = new char[N][M];
+            colorCount = new int[N][3]; // W, B, R 각각 필요 변경 횟수 저장
 
-            // 깃발 정보 입력
+            // 입력받으면서 각 행에서 색 변경 횟수 미리 계산
             for (int i = 0; i < N; i++) {
                 String str = sc.next();
                 for (int j = 0; j < M; j++) {
                     colors[i][j] = str.charAt(j);
+                    if (colors[i][j] != 'W') colorCount[i][0]++;
+                    if (colors[i][j] != 'B') colorCount[i][1]++;
+                    if (colors[i][j] != 'R') colorCount[i][2]++;
                 }
             }
 
             minCnt = Integer.MAX_VALUE;
-            divideAndCount(0, N);
-
+            divideAndCount();
             System.out.println("#" + test_case + " " + minCnt);
         }
     }
 
-    // 세 구간을 나누는 함수
-    static void divideAndCount(int start, int end) {
-        // 첫 번째 구간의 끝을 설정
-        for (int whiteEnd = start + 1; whiteEnd < end - 1; whiteEnd++) {
-            // 두 번째 구간의 끝을 설정
-            for (int blueEnd = whiteEnd + 1; blueEnd < end; blueEnd++) {
-                // 색칠 횟수 계산
+    // 색칠하는 최소 변경 횟수 계산
+    static void divideAndCount() {
+        // W(흰색) -> B(파란색) -> R(빨간색) 구간을 나누는 방식
+        for (int whiteEnd = 1; whiteEnd < N - 1; whiteEnd++) { // 흰색 구간 끝
+            for (int blueEnd = whiteEnd + 1; blueEnd < N; blueEnd++) { // 파란색 구간 끝
                 int changes = 0;
-                changes += countChanges(start, whiteEnd, 'W'); // 첫 번째 구간: 흰색
-                changes += countChanges(whiteEnd, blueEnd, 'B'); // 두 번째 구간: 파란색
-                changes += countChanges(blueEnd, end, 'R'); // 세 번째 구간: 빨간색
 
-                // 최소 변경 횟수 갱신
+                // O(1)로 구간별 변경 횟수 계산
+                changes += getColorChanges(0, whiteEnd, 0); // 흰색 구간
+                changes += getColorChanges(whiteEnd, blueEnd, 1); // 파란색 구간
+                changes += getColorChanges(blueEnd, N, 2); // 빨간색 구간
+
                 minCnt = Math.min(minCnt, changes);
             }
         }
     }
 
-    // 구간에 대해 색을 맞추는 데 필요한 색칠 횟수 계산
-    static int countChanges(int start, int end, char color) {
-        int changes = 0;
+    // 특정 색으로 바꿔야 하는 칸의 개수를 O(1)로 계산
+    static int getColorChanges(int start, int end, int color) {
+        int sum = 0;
         for (int i = start; i < end; i++) {
-            for (int j = 0; j < M; j++) {
-                if (colors[i][j] != color) {
-                    changes++;
-                }
-            }
+            sum += colorCount[i][color];
         }
-        return changes;
+        return sum;
     }
 }
-
-// 흰, 빨, 파 연속구간으로 나누기
-// 각 나눈 구간에서 변경해야 할 칸의 개수 세기
-// 최솟값 갱신
