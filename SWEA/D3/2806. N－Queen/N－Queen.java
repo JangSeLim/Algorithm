9334bbb20ad61873facdc9e7e256e71642d34086
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-12 16:45:30 +0900

    [D3] Title: N-Queen, Time: 146 ms, Memory: 29,808 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,50 +1,57 @@
 import java.util.Scanner;
 
 class Solution {
-	static int[] arr;
-	static int N;
-	static int cnt;
+    static int N;
+    static int[][] board;
+    static int cnt;
 
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int tc = 1; tc <= T; tc++) {
-			N = sc.nextInt();
-			arr = new int[N];
-			cnt = 0;
-			
-			nQueen(0);
-			System.out.println("#" + tc + " " + cnt);
-		}
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            N = sc.nextInt();
+            board = new int[N][N];
+            cnt = 0;
+            
+            nQueen(0); // 0번째 행부터 탐색 시작
+            System.out.println("#" + tc + " " + cnt);
+        }
+        sc.close();
+    } // main
 
-	} // main
+    // 백트래킹을 활용한 N-Queen 풀이
+    static void nQueen(int row) {
+        if (row == N) { // 모든 행에 퀸을 배치했다면
+            cnt++;
+            return;
+        }
 
-	static void nQueen(int idx) {
-		if (idx == N) {
-			cnt++;
-			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			arr[idx] = i;
-			if (possibility(idx)) {
-				nQueen(idx + 1);
-			}
-		}
-	}
-	
-	static boolean possibility(int col) {
-		for (int i = 0; i < col; i++) {
-			if (arr[col] == arr[i]) {
-				return false;
-			}
-			
-			else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
+        for (int col = 0; col < N; col++) { // 현재 행에서 가능한 모든 열 탐색
+            if (isSafe(row, col)) { // 유효한 위치인지 확인
+                board[row][col] = 1; // 퀸 배치
+                nQueen(row + 1); // 다음 행으로 이동
+                board[row][col] = 0; // 백트래킹
+            }
+        }
+    }
 
+    // 퀸을 배치할 수 있는지 확인하는 함수
+    static boolean isSafe(int row, int col) {
+        // 1. 같은 열(col)에 퀸이 있는지 확인
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 1) return false;
+        }
+
+        // 2. 왼쪽 위 대각선 (\ 방향) 확인
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1) return false;
+        }
+
+        // 3. 오른쪽 위 대각선 (/ 방향) 확인
+        for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
+            if (board[i][j] == 1) return false;
+        }
+
+        return true;
+    }
 }
