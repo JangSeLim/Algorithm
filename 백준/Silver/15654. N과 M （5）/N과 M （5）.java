816298ceb2615db93282aef42bc1763fd813a212
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-19 17:04:58 +0900

    [Silver III] Title: N과 M (5), Time: 368 ms, Memory: 32624 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,50 +1,44 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-    static int N, M;
-    static int[] arr; // 내림차순 정렬을 위해 Integer 배열 사용
-    static boolean[] visited; // 방문 체크
-    static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] arr, nums;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[N];
-        visited = new boolean[N]; // 방문 여부 체크 배열
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
+		nums = new int[M];
+		visited = new boolean[N];
+		recur(0);
+		System.out.println(sb);
+	}
 
-        // 입력값을 내림차순으로 정렬
-        Arrays.sort(arr);
+	static void recur(int idx) {
+		if (idx == M) {
+			for (int n : nums) {
+				sb.append(n).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
 
-        ArrayList<Integer> list = new ArrayList<>();
-        recur(0, list);
-        System.out.println(sb);
-    }
-
-    static void recur(int depth, ArrayList<Integer> list) {
-        if (list.size() == M) {
-            for (int num : list) {
-                sb.append(num).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) { // 중복 방지를 위해 방문 여부 체크
-                visited[i] = true; // 방문 표시
-                list.add(arr[i]);
-                recur(depth + 1, list);
-                list.remove(list.size() - 1);
-                visited[i] = false; // 백트래킹 시 원상 복구
-            }
-        }
-    }
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				nums[idx] = arr[i];
+				recur(idx + 1);
+				visited[i] = false;
+			}
+		}
+	}
 }
