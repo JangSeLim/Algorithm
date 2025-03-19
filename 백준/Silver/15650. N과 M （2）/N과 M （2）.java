2855ef4587dc01e2f8730acd371ddeace1549f04
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-19 16:51:11 +0900

    [Silver III] Title: N과 M (2), Time: 176 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,43 +2,31 @@ import java.util.Scanner;
 
 public class Main {
 	static int N, M;
-	static int[] arr;
-	static boolean[] visited;
-
-	public static void main(String[] args) {
+	static int[] nums;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-
 		N = sc.nextInt();
 		M = sc.nextInt();
-
-		arr = new int[M + 1];
-		visited = new boolean[N + 1];
-
-		recur(0);
+		nums = new int[M];
+		
+		recur(0, 1);
+		System.out.println(sb);
 	}
-
-	static void recur(int index) {
-		// M개의 숫자를 모두 선택한 경우
-		if (index == M) {
-			// 현재 선택한 숫자 출력
-			for (int i = 0; i < M; i++) {
-				System.out.print(arr[i] + " ");
+	
+	static void recur(int idx, int start) {
+		if (idx == M) {
+			for (int n : nums) {
+				sb.append(n).append(" ");
 			}
-			System.out.println();
-			return; // 재귀 종료
+			sb.append("\n");
+			return;
 		}
-		// 1부터 N까지 숫자를 하나씩 선택
-		for (int i = 1; i <= N; i++) {
-			// 아직 사용되지 않은 숫자인 경우
-			if (!visited[i]) {
-				visited[i] = true; // 숫자 사용 표시
-				arr[index] = i; // 선택한 숫자를 배열에 저장
-				recur(index + 1); // 다음 단계로 재귀 호출
-				// i보다 큰 수부터 false처리
-				for (int j = i + 1; j <= N; j++) {
-					visited[j] = false; // 백트래킹: 원상 복구
-				}
-			}
+		
+		for (int i = start ; i <= N; i++) {
+				nums[idx] = i;
+				recur(idx+1, i+1);
 		}
 	}
 }
