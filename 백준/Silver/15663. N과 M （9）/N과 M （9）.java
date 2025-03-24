aa273bf0e163444285a263d3e071e50956ba7c23
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-24 17:21:25 +0900

    [Silver II] Title: N과 M (9), Time: 244 ms, Memory: 23760 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,51 +1,46 @@
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-
 	static int N, M;
 	static int[] arr, nums;
 	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		nums = new int[N];
+		arr = new int[N];
 		for (int i = 0; i < N; i++) {
-			nums[i] = sc.nextInt();
+			arr[i] = sc.nextInt();
 		}
-		Arrays.sort(nums);
+		Arrays.sort(arr);
 
-		arr = new int[M];
+		nums = new int[M];
 		visited = new boolean[N + 1];
 		recur(0);
 		System.out.println(sb);
-
 	}
 
 	static void recur(int idx) {
 		if (idx == M) {
-			for (int val : arr) {
-				sb.append(val).append(" ");
+			for (int n : nums) {
+				sb.append(n).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
-
-		int before = 0; // 중복되는 수열을 배제시키기 위해
+		
+		int before = 0; // 중복되는 수열 배제
 		for (int i = 0; i < N; i++) {
 			if (visited[i]) continue;
 			
-			if (before != nums[i]) {
+			if (before != arr[i]) {
 				visited[i] = true;
-				arr[idx] = nums[i];
-				before = nums[i];
-				recur(idx+1);
+				nums[idx] = arr[i];
+				before = arr[i];
+				recur(idx + 1);
 				visited[i] = false;
 			}
 		}
@@ -53,4 +48,3 @@ public class Main {
 	}
 
 }
-// 중복 순열
