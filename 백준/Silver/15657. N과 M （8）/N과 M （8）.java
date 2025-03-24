5ae6906ec7a7092b078d43a06c8916844cb90321
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-24 16:59:02 +0900

    [Silver III] Title: N과 M (8), Time: 224 ms, Memory: 22320 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,48 +1,41 @@
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int N;
-	static int M;
-	static int[] arr;
-	static int[] result;
-	static int[] visited;
-	static StringBuilder sb = new StringBuilder(); // 시간초과 해결용
-
-	public static void main(String[] args) throws IOException{
+	static int N, M;
+	static int[] nums;
+	static int[] sel;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		
-		arr = new int[N];
-		for (int i = 0 ; i< N; i++) {
-			arr[i] = sc.nextInt();
+		nums = new int[N];
+		for (int i = 0; i < N; i++) {
+			nums[i] = sc.nextInt();
 		}
-		Arrays.sort(arr);
+		Arrays.sort(nums);
 		
-		result = new int[M];
-		perm(0, 0);
-		System.out.println(sb);
+		sel = new int[M];
+		recur(0, 0);
+		System.out.println(sb.toString());
 	}
 	
-	static void perm(int level, int start) {
-		if (level == M) {
-			for (int k = 0; k < M; k++) {
-				sb.append(result[k] + " ");
+	static void recur(int idx, int start) {
+		if (idx == M) {
+			for (int n : sel) {
+				sb.append(n).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
 		
 		for (int i = start; i < N; i++) {
-			// 이전 값과 동일한 값이 있을 경우 건너뛰기
-			if (i > start && arr[i] == arr[i-1]) continue;
-			
-			result[level] = arr[i];
-			perm(level + 1, i);
+			sel[idx] = nums[i];
+			recur(idx+1, i);
 		}
 		
 	}
-
+	
 }
