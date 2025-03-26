786e1fa2718b05e95428c41ab296adb80a20badd
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-26 14:32:52 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 130 ms, Memory: 29,952 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,66 +1,51 @@
-// 동적계획법
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-    static int[] fee; // 요금제
-    static int[] swim; // 월 별 이용 계획
-    static int min; // 가장 적은 비용
+	static int[] fee; // 요금제
+	static int[] swim; // 월 별 이용 계획
+	static int min; // 가장 적은 비용
 
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
-        for (int tc = 1; tc <= T; tc++) {
-            StringBuilder sb = new StringBuilder();
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			StringBuilder sb = new StringBuilder();
 
-            // 입력 받기
-            fee = new int[4];
-            for (int i = 0; i < 4; i++) {
-                fee[i] = sc.nextInt();
-            }
-            swim = new int[12];
-            for (int i = 0; i < 12; i++) {
-                swim[i] = sc.nextInt();
-            }
+			// 입력 받기
+			fee = new int[4];
+			for (int i = 0; i < 4; i++) {
+				fee[i] = sc.nextInt();
+			}
+			swim = new int[12];
+			for (int i = 0; i < 12; i++) {
+				swim[i] = sc.nextInt();
+			}
 
-            // 1년권 비용으로 초기화
-            min = fee[3];
+			min = fee[3];
+			dfs(0, 0); // 0번째 달부터 시작, 0원부터 시작
 
-            // dp 배열 선언
-            int[] dp = new int[12];
+			sb.append("#").append(tc).append(" ").append(min);
+			System.out.println(sb.toString());
+		}
+		sc.close();
+	}
 
-            for (int i = 0; i < 12; i++) {
-                if (swim[i] == 0) {
-                    dp[i] = (i > 0) ? dp[i - 1] : 0;  // 수영을 안하는 달은 이전 달과 같음
-                    continue;
-                }
+	static void dfs(int cnt, int sum) {
+		// 12개월을 모두 탐색한 경우
+		if (cnt >= 12) {
+			min = Math.min(min, sum); // 최소 비용 갱신
+			return;
+		}
 
-                // 1일권: swim[i] * fee[0]
-                int dayFee = swim[i] * fee[0];
-                // 한달권: fee[1]
-                int monthFee = fee[1];
+		// 1일권을 사용하는 경우
+		dfs(cnt + 1, sum + (swim[cnt] * fee[0]));
 
-                // 이번 달에 대한 최소 요금
-                dp[i] = Math.min(dayFee, monthFee);
+		// 한 달권을 사용하는 경우
+		dfs(cnt + 1, sum + fee[1]);
 
-                // 이전 달과 비교하여 누적 요금 계산
-                if (i > 0) {
-                    dp[i] += dp[i - 1];
-                }
+		// 3개월권을 사용하는 경우 (3개월권을 적용할 수 있다면)
+		dfs(cnt + 3, sum + fee[2]);
 
-                // 3개월권을 적용할 수 있는 경우 (i >= 2일 때만)
-                if (i >= 2) {
-                    dp[i] = Math.min(dp[i], (i >= 3 ? dp[i - 3] : 0) + fee[2]);
-                }
-                
-            }
-            // 최종적으로 1년권과 비교하여 최저 요금 구하기
-            min = Math.min(min, dp[11]);
-
-            sb.append("#").append(tc).append(" ").append(min);
-            System.out.println(sb.toString());
-        }
-        sc.close();
-    }
+	}
 }
