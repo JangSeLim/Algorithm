2582c85ba35e98f11e0e51fa745f726d05db08ec
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-03-28 11:59:48 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 488 ms, Memory: 124,208 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,84 +1,79 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
-
-    static class Tunnel {
-        int v1, v2;
+    static class Edge implements Comparable<Edge> {
+        int to;
         long cost;
 
-        Tunnel(int v1, int v2, long cost) {
-            this.v1 = v1;
-            this.v2 = v2;
+        Edge(int to, long cost) {
+            this.to = to;
             this.cost = cost;
         }
-    }
 
-    static long answer;
-    static int[] p;
-    static int[] x, y;
-    static ArrayList<Tunnel> arr;
+        @Override
+        public int compareTo(Edge o) {
+            return Long.compare(this.cost, o.cost);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         int T = sc.nextInt(); // 테스트 케이스 개수 입력
+        
         for (int tc = 1; tc <= T; tc++) {
             int N = sc.nextInt(); // 섬의 수
-            x = new int[N];
+            int[] x = new int[N];
+            int[] y = new int[N];
+            
             for (int i = 0; i < N; i++) {
                 x[i] = sc.nextInt();
             }
-            y = new int[N];
             for (int i = 0; i < N; i++) {
                 y[i] = sc.nextInt();
             }
             double e = sc.nextDouble();
-
-            answer = 0;
-            p = new int[N];
+            
+            // 인접 리스트 생성
+            List<Edge>[] graph = new ArrayList[N];
             for (int i = 0; i < N; i++) {
-                p[i] = i;
+                graph[i] = new ArrayList<>();
             }
-
-            arr = new ArrayList<>();
+            
             for (int i = 0; i < N; i++) {
-                for (int j = i + 1; j < N; j++) {  // i == j 방지
+                for (int j = i + 1; j < N; j++) {
                     long dist = (long) (x[i] - x[j]) * (x[i] - x[j]) + 
                                 (long) (y[i] - y[j]) * (y[i] - y[j]);
-                    arr.add(new Tunnel(i, j, dist));
+                    graph[i].add(new Edge(j, dist));
+                    graph[j].add(new Edge(i, dist));
                 }
             }
-
-            Collections.sort(arr, (o1, o2) -> Long.compare(o1.cost, o2.cost));
-
-            int cnt = 0;
-            for (Tunnel ob : arr) {
-                int pa = findSet(ob.v1);
-                int pb = findSet(ob.v2);
-                if (pa != pb) {
-                    answer += ob.cost;
-                    union(ob.v1, ob.v2);
-                    cnt++;
-                    if (cnt == N - 1) break;
+            
+            // 프림 알고리즘 (우선순위 큐 활용)
+            boolean[] visited = new boolean[N];
+            PriorityQueue<Edge> pq = new PriorityQueue<>();
+            pq.add(new Edge(0, 0)); // 임의의 시작점
+            long answer = 0;
+            int count = 0;
+            
+            while (!pq.isEmpty() && count < N) {
+                Edge current = pq.poll();
+                if (visited[current.to]) continue;
+                
+                visited[current.to] = true;
+                answer += current.cost;
+                count++;
+                
+                for (Edge next : graph[current.to]) {
+                    if (!visited[next.to]) {
+                        pq.add(next);
+                    }
                 }
             }
-
+            
             sb.append("#").append(tc).append(" ").append(Math.round(answer * e)).append("\n");
         }
         System.out.println(sb.toString());
         sc.close();
-    }
-
-    static int findSet(int v) {
-        if (v != p[v]) p[v] = findSet(p[v]); // 경로 압축 적용
-        return p[v];
-    }
-
-    static void union(int a, int b) {
-        int pa = findSet(a);
-        int pb = findSet(b);
-        if (pa != pb) p[pa] = pb;
     }
 }
