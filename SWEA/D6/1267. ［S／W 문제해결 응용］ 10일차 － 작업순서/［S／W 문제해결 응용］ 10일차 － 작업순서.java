fe9980ad510e2bb58dc8dc44aee893fe40c2dd2b
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-04-01 12:31:34 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 178 ms, Memory: 43,920 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -1,14 +1,22 @@
-// 위상정렬: 큐
-// 인접행렬
+// 위상정렬: 스택 -> dfs로 구현
+// 인접리스트
 // 사이클은 존재하지 않는다.
 // 정점의 개수 V(3 ≤ V ≤ 1000), 정점은 1부터 시작
 // 간선의 개수 E(2 ≤ E ≤ 3000)
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Solution {
+
+	static int V, E;
+	static List<Integer>[] adj;
+	static boolean[] visited;
+	static Stack<Integer> ans;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -17,49 +25,53 @@ public class Solution {
 			StringBuilder sb = new StringBuilder();
 			sb.append("#").append(tc).append(" ");
 
-			int V = sc.nextInt();
-			int E = sc.nextInt();
+			V = sc.nextInt();
+			E = sc.nextInt();
 
-			int[][] adj = new int[V + 1][V + 1];
+			adj = new ArrayList[V + 1];
+			for (int i = 1; i < V + 1; i++) {
+				adj[i] = new ArrayList<>();
+			}
+
 			int[] degree = new int[V + 1]; // 진입차수 저장
+			visited = new boolean[V + 1];
+			ans = new Stack<>();
 
 			for (int i = 0; i < E; i++) {
 				int s = sc.nextInt();
 				int e = sc.nextInt();
 
-				adj[s][e] = 1;
+				adj[s].add(e);
 				degree[e]++;
 			}
-
-			Queue<Integer> queue = new LinkedList<>();
 
 			// 진입차수가 0이면 큐에 넣기 (시작 점)
 			for (int i = 1; i < V + 1; i++) {
 				if (degree[i] == 0) {
-					queue.add(i);
+					dfs(i);
 				}
 			}
 
-			// 큐가 공백상태가 될 때까지 반복
-			while (!queue.isEmpty()) {
-				int curr = queue.poll();
-				sb.append(curr).append(" ");
-
-				// 인접행렬로 연결된 간선 확인하기 + 간선을 -1하고 0이면 큐에 넣기
-				for (int to = 1; to < V + 1; to++) {
-					if (adj[curr][to] == 1) {
-						degree[to]--;
-						if (degree[to] == 0) {
-							queue.add(to);
-						}
-					}
-				}
-			} // while
-
+			// 출력
+			while (!ans.isEmpty()) {
+				sb.append(ans.pop()).append(" ");
+			}
 			System.out.println(sb.toString());
 
 		} // tc
 
 	} // main
+
+	// v: 현재 방문 시작점
+	static void dfs(int v) {
+		visited[v] = true;
+		
+		for (int to : adj[v]) {
+			if (!visited[to]) {
+				dfs(to);
+			}
+		}
+		ans.push(v);
+	} //dfs
 
 }
