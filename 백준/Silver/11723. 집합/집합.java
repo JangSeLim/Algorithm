c925f8ca07f0eaed2d2c0faf446b2381582493a4
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-04-02 16:45:40 +0900

    [Silver V] Title: 집합, Time: 1456 ms, Memory: 313940 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -4,10 +4,11 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
+	static HashSet<Integer> set = new HashSet<>();
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		HashSet<Integer> set = new HashSet<>();
 		int M = Integer.parseInt(br.readLine()); // 연산의 수
 		
 		for (int i = 0; i < M; i++) {
@@ -16,32 +17,44 @@ public class Main {
 			int x = 0;
 			if (st.hasMoreTokens()) x = Integer.parseInt(st.nextToken());
 			
-			switch (command) {
-			case "add":
-				set.add(x);
-				break;
-			case "remove":
-				set.remove(x);
-				break;
-			case "check":
-				if (set.contains(x))
-					sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-				break;
-			case "toggle":
-				if (set.contains(x)) set.remove(x);
-				else set.add(x);
-				break;
-			case "all":
-				set.clear();
-				for(int n = 1; n <= 20; n++) set.add(n);
-				break;
-			case "empty":
-				set.clear();
-				break;
-			}
+			if (command.equals("add")) add(x);
+			else if(command.equals("remove")) remove(x);
+			else if (command.equals("check")) sb.append(check(x)).append("\n");
+			else if(command.equals("toggle")) toggle(x);
+			else if (command.equals("all")) all();
+			else if (command.equals("empty")) empty();
+			
 		}
 		
 		System.out.println(sb);
 	} // main
+	
+	static void add(int x) {
+		set.add(x);
+	}
+	
+	static void remove(int x) {
+		set.remove(x);
+	}
+	
+	static int check(int x) {
+		if (set.contains(x)) return 1;
+		else return 0;
+	}
+	
+	static void toggle(int x) {
+		if (set.contains(x)) set.remove(x);
+		else set.add(x);
+	}
+	
+	static void all() {
+		set.clear();
+		for (int i = 1; i <= 20; i++) {
+			set.add(i);
+		}
+	}
+	
+	static void empty() {
+		set.clear();
+	}
 }
