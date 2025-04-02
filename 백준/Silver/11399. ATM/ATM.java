742c3c22af33e22c55f4053401ffd53b401b9d19
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-04-02 17:18:57 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,15 +1,19 @@
 // 돈을 인출하는데 필요한 시간 합의 최솟값
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 사람의 수
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[] time = new int[N+1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			time[i] = sc.nextInt();
+			time[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		// 정렬
