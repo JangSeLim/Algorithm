f64a389d3f79dd9e4b073948899d583a9cacc0f3
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-04-15 17:48:47 +0900

    [Silver II] Title: 마인크래프트, Time: 340 ms, Memory: 36684 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -6,54 +6,61 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int N = Integer.parseInt(st.nextToken()); // 세로
 		int M = Integer.parseInt(st.nextToken()); // 가로
 		int B = Integer.parseInt(st.nextToken()); // 인벤토리 블록 개수
-		int[][] map = new int[N][M];
-		int min = 256; // 최소 높이
-		int max = 0; // 최대 높이
+
+		int[] count = new int[257]; // 높이별 블록 개수 저장 (0 ~ 256)
+		int totalBlocks = B; // 전체 블록 수 = 인벤토리 + 땅에 깔린 블록 수
+		int min = 256;
+		int max = 0;
+
+		// 입력 받으면서 count 배열에 각 높이 개수 저장
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (min > map[i][j]) min = map[i][j];
-				if (max < map[i][j]) max = map[i][j];
+				int h = Integer.parseInt(st.nextToken());
+				count[h]++;
+				totalBlocks += h;
+				min = Math.min(min, h); // 최소 높이
+				max = Math.max(max, h); // 최대 높이
 			}
 		}
-		
-		int time = Integer.MAX_VALUE; // 최소 시간 저장
-		int high = 0;
-		for (int h = min ; h <= max; h++) {
-			int cnt = 0;
-			int block = B;
-			
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					//현재 좌표의 층이 만들 층보다 높으면 제거하는데, 블록은 제거한 만큼 추가, 시간은 두배로
-					if (h < map[i][j]) {
-						cnt += ((map[i][j] - h) * 2);
-						block += (map[i][j] - h);
-					} 
-					//낮을 경우 블록을 추가, 시간은 1배
-					else {
-						cnt += (h - map[i][j]);
-						block -= (h - map[i][j]);
-					}
-					
+
+		int bestTime = Integer.MAX_VALUE; // 최소 시간
+		int bestHeight = 0;               // 그 때의 높이
+
+		// 가능한 목표 높이 h를 모두 시도 (min ~ max)
+		for (int target = min; target <= max; target++) {
+			int remove = 0; // 제거해야 할 블록 수
+			int add = 0;    // 추가해야 할 블록 수
+
+			// 각 높이 h에서 target으로 맞추기 위해 필요한 작업 계산
+			for (int h = 0; h <= 256; h++) {
+				int diff = h - target;
+				if (diff > 0) {
+					// 현재 높이가 목표보다 높으면 블록 제거
+					remove += diff * count[h];
+				} else if (diff < 0) {
+					// 현재 높이가 목표보다 낮으면 블록 추가
+					add -= diff * count[h];
 				}
 			}
-			
-			// 블록이 마이너스이면 반복문 종료 
-			if (block < 0) break;
-			
-			// 최소 시간으로 갱신
-			if (time >= cnt) {
-				time = cnt;
-				high = h;
+
+			// 제거한 블록 + 인벤토리로 추가할 블록을 충당할 수 있는 경우만 처리
+			if (remove + B >= add) {
+				int time = remove * 2 + add; // 제거: 2초, 추가: 1초
+
+				// 더 빠른 시간 or 시간이 같을 경우 더 높은 높이를 선택
+				if (time < bestTime || (time == bestTime && target > bestHeight)) {
+					bestTime = time;
+					bestHeight = target;
+				}
 			}
 		}
-		
-		System.out.println(time + " " + high);
-		
-	} // main
+
+		// 결과 출력: 최소 시간과 그때의 높이
+		System.out.println(bestTime + " " + bestHeight);
+	}
 }
