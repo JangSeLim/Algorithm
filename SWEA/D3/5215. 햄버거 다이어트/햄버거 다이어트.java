00639ec01949164549563234c62a2239d016e06d
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-04-22 17:36:53 +0900

    [D3] Title: 햄버거 다이어트, Time: 123 ms, Memory: 37,312 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,9 +4,8 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int N, L, max;
+	static int N, L;
 	static int[] score, cal;
-	static boolean[] selected;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,43 +16,28 @@ public class Solution {
 			L = Integer.parseInt(st.nextToken()); // 제한 칼로리
 			score = new int[N]; // 점수
 			cal = new int[N]; // 칼로리
-			for (int i = 0 ; i < N; i++) {
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				score[i] = Integer.parseInt(st.nextToken());
 				cal[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			max = Integer.MIN_VALUE;
-			selected = new boolean[N];
-			dfs(0);
 
-			System.out.println("#" + tc + " " + max);
-		} // tc
-	} // main
+			int[][] dp = new int[N + 1][L + 1];
+			for (int i = 1; i <= N; i++) {
+				for (int j = 0; j <= L; j++) {
+					// i번째 재료를 사용하지 않는 경우
+					dp[i][j] = dp[i - 1][j];
 
-	static void dfs(int idx) {
-		if (idx == N) {
-			int totalCal = 0;
-			int totalScore = 0;
-			
-			for (int i = 0; i < N; i++) {
-				if (selected[i]) {
-					totalCal += cal[i];
-					totalScore += score[i];
+					// i번째 재료를 사용할 수 있는 경우
+					if (j >= cal[i - 1]) {
+						int sel = dp[i - 1][j - cal[i - 1]] + score[i - 1];
+						dp[i][j] = Math.max(dp[i][j], sel);
+					}
 				}
 			}
-			
-			if (totalCal <= L) {
-				max = Math.max(max, totalScore);
-			}
-			return;
-		}
-		
-		selected[idx] = true;
-		dfs(idx + 1);
-		
-		selected[idx] = false;
-		dfs(idx + 1);
-	}
+
+			System.out.println("#" + tc + " " + dp[N][L]);
+		} // tc
+	} // main
 
 }
