c787f06f9ba4c50a6b32476eae387229ab1a8430
limielife <125421793+JangSeLim@users.noreply.github.com>
2025-04-22 17:28:36 +0900

    [D3] Title: 햄버거 다이어트, Time: 123 ms, Memory: 37,312 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,40 +3,57 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
 
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); // 재료의 개수
-            int L = Integer.parseInt(st.nextToken()); // 제한 칼로리
+	static int N, L, max;
+	static int[] score, cal;
+	static boolean[] selected;
 
-            int[] scores = new int[N]; // 맛 점수
-            int[] cals = new int[N];   // 칼로리
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 재료의 수
+			L = Integer.parseInt(st.nextToken()); // 제한 칼로리
+			score = new int[N]; // 점수
+			cal = new int[N]; // 칼로리
+			for (int i = 0 ; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				score[i] = Integer.parseInt(st.nextToken());
+				cal[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			max = Integer.MIN_VALUE;
+			selected = new boolean[N];
+			dfs(0);
 
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                scores[i] = Integer.parseInt(st.nextToken()); // 맛 점수
-                cals[i] = Integer.parseInt(st.nextToken());   // 칼로리
-            } // 입력 완료
+			System.out.println("#" + tc + " " + max);
+		} // tc
+	} // main
 
-            int[][] dp = new int[N + 1][L + 1]; // DP
+	static void dfs(int idx) {
+		if (idx == N) {
+			int totalCal = 0;
+			int totalScore = 0;
+			
+			for (int i = 0; i < N; i++) {
+				if (selected[i]) {
+					totalCal += cal[i];
+					totalScore += score[i];
+				}
+			}
+			
+			if (totalCal <= L) {
+				max = Math.max(max, totalScore);
+			}
+			return;
+		}
+		
+		selected[idx] = true;
+		dfs(idx + 1);
+		
+		selected[idx] = false;
+		dfs(idx + 1);
+	}
 
-            // 0번째 행은 아무것도 사용하지 않은 경우 (초기화 불필요)
-            for (int i = 1; i <= N; i++) {
-                for (int l = 0; l <= L; l++) {
-                    // 현재 재료를 선택하지 않는 경우 (이전 값 유지)
-                    dp[i][l] = dp[i - 1][l];
-
-                    // 현재 재료를 선택할 수 있는 경우 (칼로리 제한 확인)
-                    if (cals[i - 1] <= l) { // i번째 재료는 배열 인덱스 (i-1)에 있음
-                        dp[i][l] = Math.max(dp[i][l], dp[i - 1][l - cals[i - 1]] + scores[i - 1]);
-                    }
-                }
-            }
-
-            System.out.println("#" + tc + " " + dp[N][L]); // 결과 출력
-        }
-    }
 }
